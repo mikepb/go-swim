5cea39a11e5cdfbe7075e8942ab9c621b644dd03
Michael Phan-Ba <michael@mikepb.com>
2015-03-11 11:18:41 -0700

    RTT implements the Jacobson/Karels algorithm for estimating round-trip // time delays

---
+++ node/rtt.go
@@ -0,0 +1,49 @@
+package node
+
+import (
+	"math"
+	"sync/atomic"
+	"time"
+)
+
+// RTT implements the Jacobson/Karels algorithm for estimating round-trip
+// time delays. The formulation used is from Section 3.5 of Computer
+// Networking: A Top-Down Approach by Kurose and Ross.
+type RTT struct {
+	mean int64
+	dev  int64
+}
+
+// Atomically read the estimated mean RTT.
+func (r *RTT) Mean() time.Duration {
+	return time.Duration(atomic.LoadInt64(&r.mean))
+}
+
+// Atomically read the estimated RTT deviation.
+func (r *RTT) Deviation() time.Duration {
+	return time.Duration(atomic.LoadInt64(&r.dev))
+}
+
+// Update the RTT estimate using a RTT sample.
+func (r *RTT) Sample(v time.Duration) {
+	r.SampleWith(v, 0.125, 0.25)
+}
+
+// Update the RTT estimate using a RTT sample with the specified parameters.
+func (r *RTT) SampleWith(v time.Duration, a, b float64) {
+	mean := atomic.LoadInt64(&r.mean)
+	dev := atomic.LoadInt64(&r.dev)
+
+	// calculate new estimates
+	newMean := int64((1.0-a)*float64(mean) + a*float64(v))
+	newDev := int64((1.0-b)*float64(dev) + b*math.Abs(float64(v)-float64(mean)))
+
+	// atomically update estimates
+	atomic.CompareAndSwapInt64(&r.mean, mean, newMean)
+	atomic.CompareAndSwapInt64(&r.dev, dev, newDev)
+}
+
+// Calculate the estimated RTT.
+func (r *RTT) Timeout() time.Duration {
+	return r.Mean() + 4*r.Deviation()
+}
---
+++ node/rtt_test.go
@@ -0,0 +1,45 @@
+package node
+
+import (
+	"testing"
+	"time"
+)
+
+func TestRTT(t *testing.T) {
+	var rtt RTT
+
+	if v := rtt.Mean(); v != 0 {
+		t.Fatalf("expected mean to be %v got %v", 0, v)
+	}
+
+	if v := rtt.Deviation(); v != 0 {
+		t.Fatalf("expected deviation to be %v got %v", 0, v)
+	}
+
+	rtt.Sample(time.Second)
+	if u, v := 125*time.Millisecond, rtt.Mean(); v != u {
+		t.Fatalf("expected mean to be %v got %v", u, v)
+	}
+	if u, v := 250*time.Millisecond, rtt.Deviation(); v != u {
+		t.Fatalf("expected deviation to be %v got %v", u, v)
+	}
+
+	rtt.SampleWith(time.Second, 0.1, 0.5)
+	if u, v := 212500*time.Microsecond, rtt.Mean(); v != u {
+		t.Fatalf("expected mean to be %v got %v", u, v)
+	}
+	if u, v := 562500*time.Microsecond, rtt.Deviation(); v != u {
+		t.Fatalf("expected deviation to be %v got %v", u, v)
+	}
+
+	for i := 0; i < 9999; i++ {
+		rtt.Sample(time.Second)
+	}
+
+	if s, u, v := time.Second-time.Microsecond, time.Second+time.Microsecond, rtt.Mean(); v < s || v > u {
+		t.Fatalf("expected mean to be within [%v...%v] got %v", t, u, v)
+	}
+	if u, v := 1*time.Microsecond, rtt.Deviation(); v > u {
+		t.Fatalf("expected deviation to be less than %v got %v", u, v)
+	}
+}
