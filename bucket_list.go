b853cb51b4dbf699b2e312a857045bf7b2bb5319
Michael Phan-Ba <michael@mikepb.com>
2015-03-11 21:11:06 -0700

    Remove unused log2ceil function

--- bucket_list.go
+++ bucket_list.go
@@ -114,23 +114,3 @@ func (l *BucketList) List() []*InternalNode {
 func (l *BucketList) Size() int {
 	return len(l.nodes)
 }
-
-var deBruijn = []int{0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4,
-	8, 31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9}
-
-// https://graphics.stanford.edu/~seander/bithacks.html#IntegerLogDeBruijn
-func log2ceil(x int) int {
-	v := uint32(x)
-
-	// round to next power of 2
-	if v&(v-1) != 0 {
-		v |= v >> 1
-		v |= v >> 2
-		v |= v >> 4
-		v |= v >> 8
-		v |= v >> 16
-		v += 1
-	}
-
-	return deBruijn[(v*0x077CB531)>>27]
-}
--- bucket_list_test.go
+++ bucket_list_test.go
@@ -74,30 +74,6 @@ func TestBucketList(t *testing.T) {
 	testListSize(t, bl, 22)
 }
 
-func TestLog2ceil(t *testing.T) {
-	fixtures := map[int]int{
-		1:    0,
-		2:    1,
-		3:    2,
-		4:    2,
-		5:    3,
-		7:    3,
-		8:    3,
-		9:    4,
-		15:   4,
-		16:   4,
-		4095: 12,
-		4096: 12,
-		4097: 13,
-	}
-	for subject, expect := range fixtures {
-		t.Logf("log(%v) = %v", subject, expect)
-		if actual := log2ceil(subject); actual != expect {
-			t.Fatalf("expected log2ceil(%v) = %v got %v", subject, expect, actual)
-		}
-	}
-}
-
 func testBucketSizes(t *testing.T, bucket *BucketList, sizes []int) {
 	if n, m := len(sizes), len(bucket.buckets); n != m {
 		t.Fatalf("expected %v buckets got %v", n, m)
