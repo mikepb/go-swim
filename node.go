dc6a02e4b234ece20dfa79430a065e4f29e0aed5
Michael Phan-Ba <michael@mikepb.com>
2015-03-16 11:08:16 -0700

    Change InternalNode to track the local time for last ack and contact times. This is because the indirect ping requests are not aligned to the protocol periods.

--- node.go
+++ node.go
@@ -1,5 +1,9 @@
 package swim
 
+import (
+	"time"
+)
+
 // Node describes a member of the group.
 type Node struct {
 	Id       uint64      // Big-endian 64-bit ID, e.g. IEEE EUI-64 format
@@ -19,10 +23,10 @@ const (
 
 // InternalNode maintains the state of a node in the failure detector.
 type InternalNode struct {
-	Incarnation Seq // Last known incarnation number
-	AckSeq      Seq // Last sequence for which the node has acknowledged
-	ContactSeq  Seq // Last sequence in which a message was sent
-	RTT         RTT // Round-trip time estimator
+	Incarnation  Seq       // Last known incarnation number
+	RTT          RTT       // Round-trip time estimator
+	LastAckTime  time.Time // Last time the node acknowledged a ping
+	LastSentTime time.Time // Last time a message was sent
 
 	Node
 	State     State  // Node membership state
