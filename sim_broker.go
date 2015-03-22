867badc2b82ef3bcda55e062aabfd2188ad421dc
Michael Phan-Ba <michael@mikepb.com>
2015-03-22 14:36:57 -0700

    SimBroker and SimTransport are part of the simulator

---
+++ sim_broker.go
@@ -0,0 +1,66 @@
+package swim
+
+import (
+	"math/rand"
+	"time"
+)
+
+const kNetDelay = 10 * time.Millisecond
+const kNetStdDev = 2 * time.Millisecond
+const kMaxMessageLen = 512
+
+// SimRouter routes messages between SimTransports for the network
+// simulator.
+type SimRouter struct {
+	Routes        map[string]*SimTransport
+	Rand          *rand.Rand
+	NetDelay      time.Duration
+	NetStdDev     time.Duration
+	MaxMessageLen int
+}
+
+// Create a new SimRouter.
+func NewSimRouter() *SimRouter {
+	return &SimRouter{
+		Routes:        make(map[string]*SimTransport),
+		Rand:          rand.New(rand.NewSource(time.Now().UnixNano())),
+		NetDelay:      kNetDelay,
+		NetStdDev:     kNetStdDev,
+		MaxMessageLen: kMaxMessageLen,
+	}
+}
+
+// Create a new SimTransport associated with the given address. If a
+// transport is already associated with the given address, the existing
+// instance is returned.
+func (r *SimRouter) NewTransport(addr string) *SimTransport {
+	t, ok := r.Routes[addr]
+	if !ok {
+		t = NewSimTransport(r)
+		r.Routes[addr] = t
+	}
+	return t
+}
+
+// Send a message to the first transport matching the addresses.
+func (r *SimRouter) SendTo(addrs []string, message *CodedMessage) error {
+
+	// delay the packet to simulate a "real" network
+	time.AfterFunc(r.Delay(), func() {
+		defer recover()
+		for _, addr := range addrs {
+			if t, ok := r.Routes[addr]; ok {
+				t.RecvCh <- message
+			}
+		}
+	})
+
+	// silently fail to simulate UDP
+	return nil
+}
+
+// Generate a normally distributed time delay with a mean of NetDelay and
+// standard deviation of NetStdDev.
+func (r *SimRouter) Delay() time.Duration {
+	return time.Duration(r.Rand.NormFloat64()*float64(r.NetStdDev)) + r.NetDelay
+}
---
+++ sim_transport.go
@@ -0,0 +1,48 @@
+package swim
+
+const kRecvChSize = 8
+
+// SimTransport implements a Transport suitable for use with the simulator.
+type SimTransport struct {
+	Router *SimRouter
+	RecvCh chan *CodedMessage
+	Closed bool
+}
+
+// Create a new SimTransport associated with the given SimRouter.
+func NewSimTransport(h *SimRouter) *SimTransport {
+	return &SimTransport{
+		Router: h,
+		RecvCh: make(chan *CodedMessage, kRecvChSize),
+	}
+}
+
+// Return the maximum message length hint configured in the SimRouter.
+func (t *SimTransport) MaxMessageLen() int {
+	return t.Router.MaxMessageLen
+}
+
+// Send a message to the transports described by the addresses using the
+// SimRouter.
+func (t *SimTransport) SendTo(addr []string, message *CodedMessage) error {
+	if t.Closed {
+		panic("closed")
+	}
+	return t.Router.SendTo(addr, message)
+}
+
+// Receive a message from the receiving message queue.
+func (t *SimTransport) Recv() (*CodedMessage, error) {
+	if t.Closed {
+		panic("closed")
+	}
+	coded := <-t.RecvCh
+	return coded, nil
+}
+
+// Close the simulated transport.
+func (t *SimTransport) Close() error {
+	t.Closed = true
+	close(t.RecvCh)
+	return nil
+}
